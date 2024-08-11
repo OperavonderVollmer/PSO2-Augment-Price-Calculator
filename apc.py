9d215dcfb881a008c39eb1a5b29c4fedc77d6049
FacundoDayne <operavgxgamer@gmail.com>
2024-08-11 21:10:06 +0800

    added ability to try again

--- apc.py
+++ apc.py
@@ -30,14 +30,14 @@ luxhalpinalefull = {
 gigasmaste = {
     'Gigas Might IV':       10,
     'Gigas Precision IV':   10,
-    'Gigas TechniqueIV':    10,
+    'Gigas Technique IV':    10,
 }
 
 glangigasmastebasic = {
     'Foundia':              10,
     'Gigas Might IV':       50,
     'Gigas Precision IV':   50,
-    'Gigas TechniqueIV':    50,
+    'Gigas Technique IV':    50,
 }
 
 glangigasmastefull = {
@@ -85,20 +85,23 @@ def calc(name, augment):
     temp = []
     print("\nPlease enter the value of the following augments (ex: 10000)\nEnter 0 if you don't plan on buying") 
     for compName, compNo in augment.items():
-        price = int(input(f"{compName}: "))
+        priceInput = input(f"{compName}: ")
+        if not priceInput:
+            priceInput = 0
+        price = int(priceInput)
         total = int(price) * int(compNo)
         temp.append([price, total])
     index = 0
     grandTotal = 0
     print("\nCost Breakdown:")
     for compName, compNo in augment.items():        
-        print(f"    {compName} {compNo} * {temp[index][0]} = {temp[index][1]}") 
+        print(f"    {compName}: {compNo} * {temp[index][0]} = {temp[index][1]}") 
         grandTotal += temp[index][1]
         index += 1
     
     print(f"""
 Grand Total to buy components {name}: {locale.format_string("%d", grandTotal, grouping=True)}
-Prices to sell for profit (with tax): {locale.format_string("%d", (grandTotal * 1.30), grouping=True)}""")
+Sell for above this price to make profit (including tax): {locale.format_string("%d", (grandTotal * 1.30), grouping=True)}""")
 
 print("Welcome to PSO2 Augment Price Calculator (APC)")
 
@@ -106,8 +109,9 @@ print("""
 How to use:
 APC multiplies the values of the current prices of the components of the selected augment (that the user inputs) by how many it requires to create the end product. At the end, it returns the total price as well as the necessary price (with tax) one needs to sell the end product to make a profit""")
 
-
-augmentSelect = input("""\nPlease select an augment
+programLoop = True
+while programLoop:
+    augmentSelect = input("""\nPlease select an augment (example: 1 for Halphinale)
     1       |   Halphinale
     2       |   Lux Halphinale (Basic Components)
     3       |   Lux Halphinale (5x Halphinale)
@@ -122,30 +126,51 @@ augmentSelect = input("""\nPlease select an augment
     12      |   Grand Dread Keeper II (5x GDKI)
 input: """)
 
-match augmentSelect:
-    case "1":
-        calc("Halphinale", halphinale)
-    case "2":
-        calc("Lux Halphinale", luxhalpinalebasic)
-    case "3":
-        calc("Lux Halphinale", luxhalpinalefull)
-    case "4":
-        calc("Gigas Maste", gigasmaste)
-    case "5":
-        calc("Glan Gigas Maste", glangigasmastebasic)
-    case "6":
-        calc("Glan Gigas Maste", glangigasmastefull)
-    case "7":
-        calc("Gladia Soul", gladiasoul)
-    case "8":
-        calc("Gran Gladia Soul", grangladiasoulbasic)
-    case "9":
-        calc("Gran Gladia Soul", grangladiasoulfull)
-    case "10":
-        calc("Grand Dread Keeper I", granddreadkeeperi)
-    case "11":
-        calc("Grand Dread Keeper II", granddreadkeeperiibasic)
-    case "12":
-        calc("Grand Dread Keeper II", granddreadkeeperiifull)
+    match augmentSelect:
+        case "1":
+            calc("Halphinale", halphinale)
+        case "2":
+            calc("Lux Halphinale", luxhalpinalebasic)
+        case "3":
+            calc("Lux Halphinale", luxhalpinalefull)
+        case "4":
+            calc("Gigas Maste", gigasmaste)
+        case "5":
+            calc("Glan Gigas Maste", glangigasmastebasic)
+        case "6":
+            calc("Glan Gigas Maste", glangigasmastefull)
+        case "7":
+            calc("Gladia Soul", gladiasoul)
+        case "8":
+            calc("Gran Gladia Soul", grangladiasoulbasic)
+        case "9":
+            calc("Gran Gladia Soul", grangladiasoulfull)
+        case "10":
+            calc("Grand Dread Keeper I", granddreadkeeperi)
+        case "11":
+            calc("Grand Dread Keeper II", granddreadkeeperiibasic)
+        case "12":
+            calc("Grand Dread Keeper II", granddreadkeeperiifull)
+        case _:
+            print("Sorry, please enter the number (1-12) corresponding to an augment")
+    aftercare = input("Would you like to go again? (Y/N)\ninput: ")
+    match aftercare:
+        case "Y":
+            programLoop = True
+            os.system('cls')
+        case "y":
+            programLoop = True
+            os.system('cls')
+        case "":
+            programLoop = True
+        case "N":
+            programLoop = False
+        case "n":
+            programLoop = False
+            os.system('cls')
+        case _:
+            print("Invalid input")
+            programLoop = False
+
 
 input("\n\nThank you for using APC\nPress any key to continue...")
